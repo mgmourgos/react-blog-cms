986dae1cccd7033097424f88802bb6109b279f75
mgmourogs@users.noreply.github.com <mgmourgos@users.noreply.github.com>
2017-09-06 17:53:18 -0700

    Removed edit/delete buttons when logged out. Removed edit in list functions

--- client/components/BlogPostList/ShowPostItem.js
+++ client/components/BlogPostList/ShowPostItem.js
@@ -1,10 +1,31 @@
-const ShowPostItem = ({onRemove=f=>f, onEdit=f=>f, title, content, author, date, id}) =>
+import { Component } from 'react'
+import { connect } from 'react-redux'
+
+const ShowPostItem = ({onRemove=f=>f, onEdit=f=>f, title, content, author, date, id, isLoggedIn}) =>
     <div className="post-item">
         <div className="post-title"><a className="post-title-link" href={'/posts/' + id}>{title}</a></div>
         <div className="post-info">Posted by {author} on {date}</div>
         <div className="post-content">{content}</div>
-        <div onClick={onRemove} className="post-rm-btn">(delete post)</div>
-        <div className="post-edit-btn start"><a className="post-btn" href={'/editpost/' + id}>(edit post)</a></div>
+
+        {/* show edit and delete buttons only when logged in */}
+        { isLoggedIn &&
+            <div>
+                <div onClick={onRemove} className="post-rm-btn">
+                    (delete post)
+                </div>
+                <div className="post-edit-btn start">
+                    <a className="post-btn" href={'/editpost/' + id}>
+                        (edit post)
+                    </a>
+                </div>
+            </div>
+        }
     </div>
 
-export default ShowPostItem
+const ShowPostItemContainer = connect(
+    state => ({
+        isLoggedIn: state.auth.isLoggedIn
+    })
+)(ShowPostItem)
+
+ export default ShowPostItemContainer
--- client/components/BlogPostList/ShowPostList.js
+++ client/components/BlogPostList/ShowPostList.js
@@ -4,48 +4,14 @@ import EditPostItem from './EditPostItem'
 
 class ShowPostList extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {editing: ""}
-        this.setEditingItem=this.setEditingItem.bind(this)
-        this.editItem=this.editItem.bind(this)
-    }
-
-    //set the id of the item currently being edited
-    setEditingItem(id) {
-        this.setState({editing: id})
-    }
-
-    //dispatch an item edit
-    //set the editing item to no id
-    editItem(new_title, new_content, id) {
-        this.props.onEdit(id, new_title, new_content)
-        this.setEditingItem("")
-    }
-
     render() {
         const { store } = this.context
         const state = store.getState()
-
         return (
             <div>
                 {state.posts.map((post,i) => {
                     var date = (new Date(post.date)).toLocaleDateString()
-                    if(post.id === this.state.editing) {
-
-                        return (
-                        <EditPostItem
-                            key={i}
-                            id = {post.id}
-                            title={post.title}
-                            author={post.author}
-                            date={post.date}
-                            content={post.content}
-                            onRemove={()=>this.props.onRemove(post.id)}
-                            onEdit={(new_title, new_content)=>this.editItem(new_title, new_content, post.id)}>
-                        </EditPostItem> )
-                    } else {
-                        return (
+                    return (
                         <ShowPostItem
                             key={i}
                             id = {post.id}
@@ -56,7 +22,6 @@ class ShowPostList extends Component {
                             onRemove={()=>this.props.onRemove(post.id)}
                             onEdit={()=>this.setEditingItem(post.id)}>
                         </ShowPostItem> )
-                    }
                 })}
             </div>
         )//return
